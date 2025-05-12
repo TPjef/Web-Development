792b275da9f433c2af8dc2ba5ec68652d0183f6a
TPjef <tammanunpimpak@gmail.com>
2025-05-12 11:52:56 +0700

    หลังแข่ง

--- buy/buy.js
+++ buy/buy.js
@@ -24,6 +24,6 @@
 
     function cancelOrder() {
       if (confirm("คุณต้องการยกเลิกการสั่งซื้อใช่หรือไม่?")) {
-        window.location.href = "http://127.0.0.1:5501/index.html"; // หรือเปลี่ยนเป็นหน้าก่อนหน้าที่เหมาะสม
+        window.location.href = "http://127.0.0.1:5500/index.html"; // หรือเปลี่ยนเป็นหน้าก่อนหน้าที่เหมาะสม
       }
     }
